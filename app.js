d02440743fa2897f779590cef041482c1bc7af9e
ORX AI <131006215+orxai@users.noreply.github.com>
2025-09-02 02:43:47 +0000

    Update the website

--- app.js
+++ app.js
@@ -167,7 +167,7 @@ document.addEventListener("DOMContentLoaded", function () {
           },
           title: {
             display: true,
-            text: "Updated on Mon Sep 01 02:57 UTC. No new papers on arXiv on Sat & Sun",
+            text: "Updated on Tue Sep 02 02:42 UTC. No new papers on arXiv on Sat & Sun",
           },
         },
         elements: {
